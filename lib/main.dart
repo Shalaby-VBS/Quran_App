cc8b8b75ee58c7cbaca4ecac4ffc86a86989bb10
Shalaby-VBS <shalaby.vbs@gmail.com>
2023-01-02 10:29:37 +0200

    Shalaby-VBS

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
